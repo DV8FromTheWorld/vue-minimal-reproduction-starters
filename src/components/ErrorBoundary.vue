d0361c2a43a1fc8adff21098dcf6a11bfed2dfa4
Austin Keener <Keeneraustin@yahoo.com>
2021-06-24 10:59:44 -0400

    Setup for simple minimal reproduction cases

---
+++ src/components/ErrorBoundary.vue
@@ -0,0 +1,27 @@
+<script>
+  import { h } from 'vue'
+
+  export default {
+    name: 'ErrorBoundary',
+    data() {
+      return {
+        error: false
+      }
+    },
+    errorCaptured (err, vm, info) {
+      this.error = true
+      console.error(`[${info}]`, err)
+
+      return false
+    },
+    render () {
+      if (this.error) {
+        return h('p', { style: 'color: red' }, 'An error occurred. Check console for information')
+      }
+
+      return this.$slots.default.length === 1
+        ? h(this.$slots.default[0])
+        : h('div', {}, this.$slots.default)
+    }
+  }
+</script>
